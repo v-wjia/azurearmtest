72a2818f66b5571c9eb34e0f008af16f0ce93892
Jia Wang (Wicresoft) <v-wjia@microsoft.com>
2016-08-31 22:42:12 +0800

    update

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-yum -y install epel-release nginx
-/etc/init.d/nginx start
+
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+
+config="
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/OS/OSRELEASE/$basearch/
+gpgcheck=0
+enabled=1
+"
+
+#echo "$config" > /etc/yum.repos.d/nginx.repo
+yum check-update
+yum install nginx
+
+
+
+service nginx start
+chkconfig nginx on
